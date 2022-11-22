9e9f4d57bae147d1828c18b908aa9df66c814554
KOBIA-m <103171586+KOBIA-m@users.noreply.github.com>
2022-11-22 16:46:00 +0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,78 +1,91 @@
-          ######################################################################
-    # DELETE AN ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["DELETE"])
-    def delete_accounts(account_id):
-        """
-        Delete an Account
-        This endpoint will delete an Account based on the account_id that is requested
-        """
-        app.logger.info("Request to delete an Account with id: %s", account_id)
+######################################################################
+# DELETE AN ACCOUNT
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
 
-        account = Account.find(account_id)
-        if account:
-            account.delete()
+    account = Account.find(account_id)
+    if account:
+        account.delete()
 
-        return "", status.HTTP_204_NO_CONTENT
-          
-         ######################################################################
-    # UPDATE AN EXISTING ACCOUNT
-    ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["PUT"])
-    def update_accounts(account_id):
-        """
-        Update an Account
-        This endpoint will update an Account based on the posted data
-        """
-        app.logger.info("Request to update an Account with id: %s", account_id)
-
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-
-        account.deserialize(request.get_json())
-        account.update()
-
-        return account.serialize(), status.HTTP_200_OK
-
-         ######################################################################
-    # LIST ALL ACCOUNTS
-    ######################################################################
-    @app.route("/accounts", methods=["GET"])
-    def list_accounts():
-        """
-        List all Accounts
-        This endpoint will list all Accounts
-        """
-        app.logger.info("Request to list Accounts")
-
-        accounts = Account.all()
-        account_list = [account.serialize() for account in accounts]
-
-        app.logger.info("Returning [%s] accounts", len(account_list))
-        return jsonify(account_list), status.HTTP_200_OK
-
-       def test_get_account_not_found(self):
-        """It should not Read an Account that is not found"""
-        resp = self.client.get(f"{BASE_URL}/0")
-        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND) 
+    return "", status.HTTP_204_NO_CONTENT
 
     ######################################################################
-    # READ AN ACCOUNT
+
+
+# UPDATE AN EXISTING ACCOUNT
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based on the posted data
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+    if not account:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.",
+        )
+
+    account.deserialize(request.get_json())
+    account.update()
+
+    return account.serialize(), status.HTTP_200_OK
+
     ######################################################################
-    @app.route("/accounts/<int:account_id>", methods=["GET"])
-    def get_accounts(account_id):
-        """
-        Reads an Account
-        This endpoint will read an Account based the account_id that is requested
-        """
-        app.logger.info("Request to read an Account with id: %s", account_id)
 
-        account = Account.find(account_id)
-        if not account:
-            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
-        return account.serialize(), status.HTTP_200_OK
+# LIST ALL ACCOUNTS
+######################################################################
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
+
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
+
+
+def test_get_account_not_found(self):
+    """It should not Read an Account that is not found"""
+    resp = self.client.get(f"{BASE_URL}/0")
+    self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
+######################################################################
+# READ AN ACCOUNT
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["GET"])
+def get_accounts(account_id):
+    """
+    Reads an Account
+    This endpoint will read an Account based the account_id that is requested
+    """
+    app.logger.info("Request to read an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+    if not account:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Account with id [{account_id}] could not be found.",
+        )
+
+    return account.serialize(), status.HTTP_200_OK
+
 
 """
 Account Service
@@ -80,7 +93,7 @@ Account Service
 This microservice handles the lifecycle of Accounts
 """
 # pylint: disable=unused-import
-from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
@@ -132,6 +145,7 @@ def create_accounts():
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
+
 
 ######################################################################
 # LIST ALL ACCOUNTS
